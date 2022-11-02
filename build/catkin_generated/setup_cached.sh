622336ec557cab9a7ecbd997fc6a27ae23329454
Junghwan <personaljh3@gmail.com>
2022-11-02 22:54:48 +0900

    isntall ros noetic and make

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/mnt/c/Users/JH/Desktop/code/Little-and-useful-/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/mnt/c/Users/JH/Desktop/code/Little-and-useful-/devel/lib:$LD_LIBRARY_PATH"
+export PWD='/mnt/c/Users/JH/Desktop/code/Little-and-useful-/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/mnt/c/Users/JH/Desktop/code/Little-and-useful-/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/mnt/c/Users/JH/Desktop/code/Little-and-useful-/main:$ROS_PACKAGE_PATH"
