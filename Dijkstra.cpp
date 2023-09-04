9ab26747872ed23eeb1026dc95f3ae458f9be86a
junghwan <personaljh3@gmail.com>
2023-09-04 15:06:46 +0900

    djikstra algorithm

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -41,8 +41,69 @@ void dijkstra(vector<vector<pair_int>>& graph, int start) {
     }
 }
 
+
+int getSmallIndex(vector<int> dis){
+
+    int min = INT_MAX;
+    int index;
+
+    for(int i = 0; i < dis.size(); i++){
+        if(min < dis.at(i)){
+            min = dis.at(i);
+            index = i ;
+        }
+    }
+    
+
+
+    return index;
+}
+
+
+
+void dijkstra2(vector<vector<pair_int>>& graph, int start) {
+    int n = graph.size();
+    //출발점이 정해져 있으니까-> 내가 원하는 것만
+    vector<int> distances(n, INT_MAX);
+    distances[start] = 0;
+    priority_queue<pair_int, vector<pair_int>, greater<pair_int>> pq;
+
+    //start num 에 있을 때 distances 를 구하기
+    //distances = [0,1,4,MAX]
+    for(int i = 0; i < graph[start].size(); i++){
+        if(distances.at(i) < graph[start].at(i).second){
+            distances.at(i) = graph[start].at(i).second;
+        }
+    }
+
+    // weight가 가장 작은 노드로 넘어가서 distances를 업데이트하기
+    for(int i = 0; i < graph.size(); i++){
+        for(auto j : graph.at(i)){
+            pq.push(j);
+        }
+
+    }
+
+
+    // 그 다음 weight로 가서 distances를 업데이트하기 
+
+    
+
+
+
+
+
+    // 최단 거리 출력
+    for (int i = 1; i < n; ++i) {
+        cout << "노드 " << i << "까지의 최단 거리: " << distances[i] << endl;
+    }
+}
+
+
+
+
 int main() {
-    int n = 4; // 노드 수
+    int n = 5; // 노드 수
     vector<vector<pair_int>> graph(n);
 
     graph[1].push_back(make_pair(2, 1));
