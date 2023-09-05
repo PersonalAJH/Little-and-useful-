10b196cd52109a454f9f52bafb5e8f3432602c20
junghwan <personaljh3@gmail.com>
2023-09-05 10:44:24 +0900

    A* algorithm

---
+++ A-start.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
+#include <cmath>
+
+
+/*
+디익스트라 알고리즘과 A*알고리즘의 차이 
+https://youtu.be/RYdBcnSiwag?si=1mYl_xonRgJ-Cs5c
+
+예시가 유튜브에 잘나와있음 
+
+디익스트라 알고리즘의 경우 휴리스틱 함수의 비교가 없기 때문에 방향성을 갖지 못해 시작점으로 부터 원형으로 탐색을 하며(시작점으로 부터 거리가 가장 가까운곳)
+A*알고리즘의 경우 방향성을 가지기 때문에 디익스트라 알고리즘보다 훨씬더 빠르게 길을 찾을 수 있다. 만약에 휴리스틱 함수의 weight? value가 모두 같다면 A* 알고리즘과 디익스트라 알고리즘은 같아지게 된다. 
+
+*/
+
+struct Node {
+    int x, y; // Node의 좌표
+    double g; // 시작 노드로부터의 비용
+    double h; // 목표 노드로부터의 예상 비용
+    double f; // f = g + h
+    Node* parent; // 이전 노드에 대한 포인터
+
+    Node(int x, int y) : x(x), y(y), g(0), h(0), f(0), parent(nullptr) {}
+
+    // 비용 계산을 위한 휴리스틱 함수 -> 대표적으로는 맨해튼, 유클리디안이 있는데 여기서는 맨해튼(계산이 편함)
+    double calculateHeuristic(const Node& goal) {
+        return std::abs(x - goal.x) + std::abs(y - goal.y);
+    }
+};
+
+// 우선순위 큐를 사용하여 노드를 정렬
+struct CompareNode {
+    bool operator()(Node* a, Node* b) {
+        return a->f > b->f;
+    }
+};
+
+std::vector<Node*> aStarSearch(Node* start, Node* goal) {
+    std::priority_queue<Node*, std::vector<Node*>, CompareNode> openSet;
+    std::map<std::pair<int, int>, Node*> nodeMap;
+
+    openSet.push(start);
+    nodeMap[std::make_pair(start->x, start->y)] = start;
+
+    while (!openSet.empty()) {
+        Node* current = openSet.top();
+        openSet.pop();
+
+        if (current == goal) {
+            // 목표에 도달한 경우 경로를 복원
+            std::vector<Node*> path;
+            while (current != nullptr) {
+                path.push_back(current);
+                current = current->parent;
+            }
+            std::reverse(path.begin(), path.end());
+            return path;
+        }
+
+        // 이웃 노드 순회
+        std::vector<std::pair<int, int>> neighbors = {
+            {current->x + 1, current->y},
+            {current->x - 1, current->y},
+            {current->x, current->y + 1},
+            {current->x, current->y - 1}
+        };
+
+        for (const auto& neighborPos : neighbors) {
+            int nx = neighborPos.first;
+            int ny = neighborPos.second;
+
+            if (nx < 0 || ny < 0) continue; // 범위를 벗어나면 무시
+
+            // 새로운 이웃 노드 생성
+            Node* neighbor = new Node(nx, ny);
+            neighbor->g = current->g + 1; // currnet 에서 neighbor 롤 움직이는 weight -> 여기서는 1로 고정
+            neighbor->h = neighbor->calculateHeuristic(*goal);
+            neighbor->f = neighbor->g + neighbor->h;
+            neighbor->parent = current;
+
+            // 이미 방문한 노드인지 확인
+            if (nodeMap.find(std::make_pair(nx, ny)) != nodeMap.end()) {
+                Node* existingNeighbor = nodeMap[std::make_pair(nx, ny)];
+                if (neighbor->f >= existingNeighbor->f) {
+                    delete neighbor;
+                    continue;
+                }
+            }
+
+            openSet.push(neighbor);
+            nodeMap[std::make_pair(nx, ny)] = neighbor;
+        }
+    }
+
+    // 목표에 도달할 수 없는 경우 빈 벡터 반환
+    return std::vector<Node*>();
+}
+
+int main() {
+    Node start(0, 0);
+    Node goal(5, 5);
+
+    std::vector<Node*> path = aStarSearch(&start, &goal);
+
+    if (!path.empty()) {
+        std::cout << "최단 경로: ";
+        for (Node* node : path) {
+            std::cout << "(" << node->x << ", " << node->y << ") -> ";
+        }
+        std::cout << "도착!" << std::endl;
+    } else {
+        std::cout << "목표에 도달할 수 없습니다." << std::endl;
+    }
+
+    // 메모리 해제
+    for (Node* node : path) {
+        delete node;
+    }
+
+    return 0;
+}
--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -5,6 +5,28 @@
 
 using namespace std;
 
+
+/*
+//설명 대강 
+// 다이나믹 프로그래밍을 이용한 최단 경로 탐색 알고리즘 
+흔히 인공위성 GPS 소프트웨어 등을 많이 사용한다. 특정한 하나의 정점과 다른 모든 정점으로 가는 최단 경로를 알려준다. 
+
+
+정확성: 다익스트라 알고리즘은 가중치가 음수가 아닌 경우에 정확한 최단 경로를 찾습니다.
+유연성: 다양한 종류의 그래프에서 사용할 수 있습니다. 방향성, 무방향성, 가중치 등 다양한 형태의 그래프에서 적용 가능합니다.
+단일 출발지: 하나의 출발 노드에서 다른 모든 노드까지의 최단 경로를 찾는 데 효과적입니다.
+간선 중복 허용: 그래프 내에서 간선의 중복을 허용하는 경우에도 정확한 결과를 제공합니다.
+데이터 구조 활용: 우선순위 큐를 사용하여 구현할 수 있어, 효율적인 데이터 구조를 활용하여 빠른 실행이 가능합니다.
+단점:
+
+음수 가중치 처리 어려움: 다익스트라 알고리즘은 음수 가중치를 처리하는 데 제약이 있습니다. 음수 가중치가 있는 경우에는 벨만-포드 알고리즘과 같은 다른 알고리즘이 필요합니다.
+최단 경로 갱신 문제: 다익스트라 알고리즘은 최단 경로를 찾은 노드의 가중치를 갱신하는 과정에서 오류가 발생할 수 있습니다. 이를 보완하기 위해 큐를 활용하는데, 이는 코드 구현에 복잡성을 추가할 수 있습니다.
+Dense 그래프에서 성능 저하: 그래프가 밀집되어있는 경우(간선이 많은 경우) 성능이 저하될 수 있습니다. 이런 경우에는 힙 대신에 배열 기반의 자료 구조를 사용하여 성능 개선을 시도할 수 있습니다.
+완전 그래프에서 성능 저하: 그래프가 완전 그래프 형태일 경우(모든 노드가 서로 연결된 경우) 효율성이 떨어질 수 있습니다.
+
+
+
+*/
 typedef pair<int, int> pair_int;
 
 void dijkstra(vector<vector<pair_int>>& graph, int start) {
@@ -50,12 +72,10 @@ int getSmallIndex(vector<int> dis){
     for(int i = 0; i < dis.size(); i++){
         if(min < dis.at(i)){
             min = dis.at(i);
-            index = i ;
+            index = i;
         }
     }
     
-
-
     return index;
 }
 
@@ -77,20 +97,20 @@ void dijkstra2(vector<vector<pair_int>>& graph, int start) {
     }
 
     // weight가 가장 작은 노드로 넘어가서 distances를 업데이트하기
-    for(int i = 0; i < graph.size(); i++){
-        for(auto j : graph.at(i)){
-            pq.push(j);
+    // 첫번째 distances = [0,1,4,MAX]
+    // 두번째 distances 는 grapg.at(t)로 들어가서 더하고 작으면 graph2 에서 (1,1),(3,2),(4,5)
+    int t;
+    t = getSmallIndex(distances);
+    graph.at(t);
+    for(int i = 0; i < graph[t].size(); i++){
+        int tmp_node = graph[t].at(i).first;
+        int tmp_weight = graph[t].at(i).second;
+        if(distances.at(t) + tmp_weight < distances.at(tmp_node)){
+            distances.at(tmp_node) = distances.at(t) + tmp_weight;
         }
-
     }
 
-
     // 그 다음 weight로 가서 distances를 업데이트하기 
-
-    
-
-
-
 
 
     // 최단 거리 출력
